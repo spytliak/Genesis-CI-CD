9b14745ac05578edede8deeefd2fc4a100212917
Serhii Pytliak <pytliak.serhii@gmail.com>
2022-07-14 23:44:22 +0300

    updated bucket - sandbox

--- terraform/project_GENESIS/backend.tf
+++ terraform/project_GENESIS/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "tfstate-project-genesis-t"
+    bucket = "tfstate-project-genesis-dev"
     key    = "genesis/app.tfstate"
     region = "us-east-1"
   }
