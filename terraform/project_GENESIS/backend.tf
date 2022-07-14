6074fbb6f5683613e406bb1c557fd7e8f30b8e86
Serhii Pytliak <pytliak.serhii@gmail.com>
2022-07-14 15:24:08 +0300

    bucket

--- terraform/project_GENESIS/backend.tf
+++ terraform/project_GENESIS/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "tfstate-project-genesis-sp"
+    bucket = "tfstate-project-genesis-spa"
     key    = "genesis/app.tfstate"
     region = "us-east-1"
   }
